177c24d2394f872f38f428ce3a2cc608e0abf6df
Zabamund <fracgeol@gmail.com>
2018-03-22 11:14:49 +0100

    node snippets

--- node_snippets/readLists.js
+++ node_snippets/readLists.js
@@ -3,4 +3,5 @@ const fs = require('fs');
 let list_content = fs.readFileSync("../DATA_format_JSON/CE/datas/2351/CE_infosListes.json");
 //console.log("Output Content : \n"+ list_content);
 let jsonContent = JSON.parse(list_content);
-console.log(jsonContent[1]["nom"]);
+console.log(jsonContent[2]['sigle']);
+console.log(jsonContent[2]['sigle']);
