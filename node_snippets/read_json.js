4a2319263f42174616587524ffa8cd43502862be
Zabamund <fracgeol@gmail.com>
2018-03-23 16:26:50 +0100

    nodejs snippets

---
+++ node_snippets/read_json.js
@@ -0,0 +1,7 @@
+let jsonObj = require('../DATA_format_JSON/CE/datas/2351/CE_Resultats_Candidat_20001.json');
+console.log('jsonObj.candidat.liste.id:')
+console.log(jsonObj.candidat.liste.id);
+console.log('jsonObj.resultatsCandidatParCommune:')
+console.log(jsonObj.resultatsCandidatParCommune);
+console.log('jsonObj.resultatsCandidatParCommune[0].commune.nom:')
+console.log(jsonObj.resultatsCandidatParCommune[0].commune.nom);
